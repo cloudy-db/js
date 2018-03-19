9752c92f50a27f0675263bac662c657db66235f9
Isaac Kwan <g@isaac.pw>
2018-03-19 18:27:07 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const IPFS = require('ipfs');
+const OrbitDB = require('orbit-db');
+
+class ezDb extends OrbitDB {
+	/**
+	 * @param {Object} ipfsOptions
+	 * @returns {ezDb}
+	 */
+	constructor(ipfsOptions) {
+		this.ipfs = 
+	}
+}
