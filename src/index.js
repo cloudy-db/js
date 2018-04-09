28aa4276130e27bd02d6d7bfab9ffbd905040c6f
Isaac Kwan <g@isaac.pw>
2018-04-09 19:01:43 +0800

    refactored constructor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const IPFS = require("ipfs");
 const OrbitDB = require("orbit-db");
 const IPFSRepo = require("ipfs-repo");
+const EventEmitter = require("events");
 
 const spOptions = {
 	trickle: true,
@@ -31,7 +32,6 @@ if (typeof self === "undefined") {
 
 /** @typedef {*} IPFS */
 /** @typedef {*} DocumentStore */
-/** @typedef {*} EventStore */
 
 /**
  * OrbitDB class {@link https://github.com/orbitdb/orbit-db}
@@ -40,80 +40,115 @@ if (typeof self === "undefined") {
  * 
  */
 
+ /**
+  * helper method to create an IPFS instance based on user-supplied params
+  * @param {Object} ipfsOrOptions options to be passed to IPFS constructor
+  * @returns {Promise<IPFS>}
+  */
+function initIpfsInstance(ipfsOrOptions) {
+	return new Promise((resolve, reject) => {
+		const wstar = new WStar({ wrtc: wrtc, spOptions: spOptions });
+		ipfsOrOptions = Object.assign({
+			repo: new IPFSRepo("./storage/ipfs-repo"),
+			config: {
+				Addresses: {
+					Swarm: [
+						// "/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star",
+						// "/ip4/0.0.0.0/tcp/" + (10000 + Math.floor(Math.random()*55535)),
+						// "/ip4/172.28.2.3/tcp/9090/ws/p2p-webrtc-star",
+						"/dns4/nas1.isaac.pw/tcp/9090/ws/p2p-webrtc-star",
+						// "/dns6/rpi-ipv6.test/tcp/9090/ws/p2p-webrtc-star",
+					],					  
+				},
+				Bootstrap: [],
+			}
+		}, ipfsOrOptions, {
+			libp2p: {
+				modules: {
+					transport: [wstar],
+					discovery: [wstar.discovery],
+				}
+			}
+		});
+		const ipfs = new IPFS(Object.assign({EXPERIMENTAL: {
+			pubsub: true,
+		}}, ipfsOrOptions));
+
+		ipfs.on("error", (e) => {
+			console.error("error in ipfs", e);
+			reject(e);
+		});
+		ipfs.on("ready", async () => {
+			/*ipfs._libp2pNode.on('peer:discovery', (peer) => {
+				console.log('Discovered:', peer.id.toB58String());
+			});*/
+			ipfs._libp2pNode.on("peer:connect", (peer) => {
+				console.log("Connection established to:", peer.id.toB58String());
+			});
+			resolve(ipfs);
+		});
+	});
+}
+
 /**
  * Super-powered OrbitDB instance
  */
-class Cloudy {
+class Cloudy extends EventEmitter {
 
 	/**
-	 * @param {IPFS} ipfs
+	 * @param {IPFS|Object} ipfsOrOptions ready-ed IPFS instance OR an object which will be passed to the IPFS constructor
 	 * @param {string} [directory]
-	 * @param {*} [options]
+	 * @param {string} [deviceId] unique device ID -- should be SAME each time user invokes the app
+	 * @param {Function} [wakeupFunction] a function to wake up the device for syncing. return a promise to pause the sync
+	 * @param {*} [options] options to pass to OrbitDB constructor
 	 */
-	constructor(ipfs, directory = "./storage/orbitdb", options) {
-		this.ipfs = ipfs;
-		/** @type {OrbitDB} */
-		// @ts-ignore
-		this.orbitDb = new OrbitDB(ipfs, directory, options);
-		this.orbitDb.eventlog("devices").then((store) => {
-			/** @type {EventStore} */
-			this.devices = store;
-		});
+	constructor(ipfsOrOptions = {}, directory = "./storage/orbitdb", deviceId, wakeupFunction, options) {
+		super();
+		if (ipfsOrOptions._libp2pNode) {
+			this.ipfs = ipfsOrOptions
+		}
+		(async () => {
+			try {
+				if (!this.ipfs) {
+					this.ipfs = await initIpfsInstance(ipfsOrOptions);
+				}
+	
+				/** @type {OrbitDB} */
+				// @ts-ignore
+				this.orbitDb = new OrbitDB(this.ipfs, directory, options);
+				/* const optsForDevicesDb = {admin: ["*"], write: ["*"]};
+				this.orbitDb.docstore("devices", optsForDevicesDb).then((store) => {
+					/** @type {DocumentStore} 
+					this.devices = store;
+					// store.
+				}); */
+	
+				this.emit("ready");
+			} catch (e) {
+				this.emit("error", e);
+			}
+		})();
 	}
 
 	/**
-	 * Preferred way of creating a Cloudy instance.
-	 * @param {*} [ipfsOptions]
+	 * Short hand to create a Cloudy instance.
+	 * @param {IPFS|Object} ipfsOrOptions ready-ed IPFS instance OR an object which will be passed to the IPFS constructor
 	 * @param {string} [directory]
-	 * @param {*} [options]
+	 * @param {string} [deviceId] unique device ID -- should be SAME each time user invokes the app
+	 * @param {Function} [wakeupFunction] a function to wake up the device for syncing. return a promise to pause the sync
+	 * @param {*} [options] options to pass to OrbitDB constructor
 	 * @return {Promise<Cloudy>} - Cloudy instance 
 	 */
-	static create(ipfsOptions = {}, directory, options) {
-		if (typeof self === "undefined") {
-			const wstar = new WStar({ wrtc: wrtc, spOptions: spOptions });
-			ipfsOptions = Object.assign({
-				repo: new IPFSRepo("./storage/ipfs-repo"),
-				config: {
-					Addresses: {
-						Swarm: [
-							// "/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star",
-							// "/ip4/0.0.0.0/tcp/" + (10000 + Math.floor(Math.random()*55535)),
-							// "/ip4/172.28.2.3/tcp/9090/ws/p2p-webrtc-star",
-							"/dns4/nas1.isaac.pw/tcp/9090/ws/p2p-webrtc-star",
-							// "/dns6/rpi-ipv6.test/tcp/9090/ws/p2p-webrtc-star",
-						],					  
-					},
-					Bootstrap: [],
-				}
-			}, ipfsOptions, {
-				libp2p: {
-					modules: {
-						transport: [wstar],
-						discovery: [wstar.discovery],
-					}
-				}
+	static create(ipfsOrOptions, directory, deviceId, wakeupFunction, options) {
+		return new Promise((resolve, reject) => {
+			const cloudy = new Cloudy(...arguments);
+			cloudy.once("ready", () => {
+				resolve(cloudy);
 			});
-		}
-		return new Promise(function(resolve, reject){
-			const ipfs = new IPFS(Object.assign({EXPERIMENTAL: {
-				pubsub: true,
-			}}, ipfsOptions));
-
-			ipfs.on("error", (e) => {
-				console.error("error in ipfs", e);
+			cloudy.once("error", (e) => {
 				reject(e);
 			});
-			ipfs.on("ready", async function() {
-				/*ipfs._libp2pNode.on('peer:discovery', (peer) => {
-					console.log('Discovered:', peer.id.toB58String());
-				});*/
-				ipfs._libp2pNode.on("peer:connect", (peer) => {
-					console.log("Connection established to:", peer.id.toB58String());
-				});
-				resolve(new Cloudy(ipfs, directory, options));
-			});
 		});
-
 	}
 
 	/**
