5a06ede0def663b98c5c856c064fb1e66fd7f8bc
Isaac Kwan <g@isaac.pw>
2018-05-12 00:11:11 +0800

    actually some progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
 	entry: "./src/index-reactnative.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js"
+		filename: "bundle.js",
+		library: "cloudy",
+		libraryTarget: "umd",
 	},
 	resolve: {
 		aliasFields: ["react-native", "browser"],
@@ -16,4 +19,35 @@ module.exports = {
 		}
 	},
 	devtool: "cheap-source-map",
+	optimization: {
+		minimizer: [
+			/*new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true,
+				uglifyOptions: {
+					safari10: false,
+					compress: {
+						inline: 0,
+					},
+					passes: 2,
+				},
+			}),*/
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /babel/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						plugins: [require("babel-plugin-transform-object-assign")],
+						babelrc: false,
+					}
+				}
+			}
+		]
+	},	  
 };
